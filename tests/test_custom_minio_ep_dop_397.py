4671e1c24aa2a01ff3f65ff0fa76d68f785034de
Dmitry Kartesv <dmitry.kartsev@cnvrg.io>
2020-10-13 17:11:05 +0300

    wip on tests

---
+++ tests/test_custom_minio_ep_dop_397.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+from common import CommonBase
+from kubernetes import client, config
+import logging
+
+config.load_kube_config()
+
+
+class CnvrgMpiOperatorDop395Test(unittest.TestCase, CommonBase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.deploy()
+        cls.create_cnvrg_spec(cls.get_spec_from_chart())
+        cls.wait_for_cnvrg_spec_ready()
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.getenv("RUN_TEARDOWN", "true") == "true":
+            cls.delete_cnvrg_spec()
+            cls.undeploy()
+
+    def test_mpi_operator_pod_deployed(self):
+        v1 = client.CoreV1Api()
+        pod = v1.list_namespaced_pod("cnvrg", label_selector="app=mpi-operator")
+        self.assertEqual(1, len(pod.items))
+
+    def test_mpi_operator_pod_ready(self):
+        cmd = "kubectl wait --for=condition=ready pod -l app=mpi-operator -ncnvrg --timeout=120s"
+        ret_code = self.exec_cmd(cmd)
+        self.assertEqual(0, ret_code)
