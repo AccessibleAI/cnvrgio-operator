5050c39dbe80f2ab2841b57bbbe686346f842c19
Dmitry Kartesv <dmitry.kartsev@cnvrg.io>
2020-10-21 09:19:07 +0300

    wip

--- tests/test_taint_dop_392.py
+++ tests/test_taint_dop_392.py
@@ -17,12 +17,94 @@ spec:
   tenancy:
     enabled: "true"
   cnvrgApp:
-    image: "core:core-3.4.1-10-300"
+    image: "cnvrg/core:3.1.3"
     intercom: "false"
   cnvrgRouter:
     enabled: "true"
   pgBackup:
     enabled: "true"
+"""
+
+CNVRG_SPEC_WITH_TOLERATION = """
+apiVersion: mlops.cnvrg.io/v1
+kind: CnvrgApp
+metadata:
+  name: cnvrg-app
+  namespace: cnvrg
+spec:
+  ingressType: "k8singress"
+  clusterDomain: "__CLUSTER_DOMAIN__"
+  tenancy:
+    enabled: "true"
+    dedicatedNodes: "true"
+  cnvrgApp:
+    enabled: "true"
+    image: "cnvrg/core:3.1.2"
+    customAgentTag: "false"
+    intercom: "false"
+  redis:
+    enabled: "true"
+  pg:
+    enabled: "true"
+  es:
+    enabled: "true"
+  minio:
+    enabled: "true"
+  prometheus:
+    enabled: "true"
+  istio:
+    enabled: "true"
+  kibana:
+    enabled: "true"
+  fluentd:
+    enabled: "true"
+  nvidiadp:
+    enabled: "false"
+  mpi:
+    enabled: "true"
+
+
+
+"""
+
+CNVRG_SPEC_WITH_TOLERATION_ISTIO_ONLY = """
+apiVersion: mlops.cnvrg.io/v1
+kind: CnvrgApp
+metadata:
+  name: cnvrg-app
+  namespace: cnvrg
+spec:
+  ingressType: "istio"
+  clusterDomain: "__CLUSTER_DOMAIN__"
+  tenancy:
+    enabled: "true"
+    dedicatedNodes: "true"
+  istio:
+    enabled: "true"
+  mpi:
+    enabled: "false"
+  conf:
+    enabled: "false"
+  conf:
+    enabled: "false"
+  cnvrgApp:
+    enabled: "false"
+  pgBackup:
+    enabled: "false"
+  redis:
+    enabled: "false"
+  es:
+    enabled: "false"
+  minio:
+    enabled: "false"
+  prometheus:
+    enabled: "false"
+  kibana:
+    enabled: "false"
+  fluentd:
+    enabled: "false"
+  nvidiadp:
+    enabled: "false"
 """
 
 
@@ -31,7 +113,7 @@ spec:
 #     @classmethod
 #     def setUpClass(cls):
 #         cls.deploy()
-#         cls._exec_cmd("kubectl label nodes cnvrg-taint=true --all --overwrite")
+#         cls._exec_cmd("label node cnvrg-taint- --all")
 #         cls.create_cnvrg_spec(CNVRG_SPEC)
 #         cls.wait_for_cnvrg_spec_ready()
 #
@@ -115,22 +197,114 @@ spec:
 #
 
 
-class CnvrgTaintsAreSetDedicatedNodesFalseTest(unittest.TestCase, CommonBase):
+# class CnvrgTaintsAreSetDedicatedNodesFalseTest(unittest.TestCase, CommonBase):
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.get_nip_nip_url()
+#         cls.deploy()
+#         cls._exec_cmd("kubectl label nodes cnvrg-taint=true --all --overwrite")
+#         cls._exec_cmd("kubectl create deployment --image=nginx -ncnvrg test-nginx")
+#         cls.create_cnvrg_spec(CNVRG_SPEC.replace("__CLUSTER_DOMAIN__", cls.get_nip_nip_url()))
+#         cls.wait_for_cnvrg_spec_ready()
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls._exec_cmd("kubectl label node cnvrg-taint- --all")
+#         cls.delete_cnvrg_spec()
+#         cls.undeploy()
+#
+#     def test_app(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=app")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_pg(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=postgres")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_es(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=elasticsearch")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_kibana(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=kibana")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_prom_operator(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app.kubernetes.io/name=prometheus-operator")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_minio(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=minio")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_mpi_operator(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=mpi-operator")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_redis(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=redis")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_grafana(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=grafana")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_cnvrg_routing(self):
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=routing-service")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
+#
+#     def test_custom_nginx_deploy(self):
+#         cmd = "kubectl wait --for=condition=ready pod -l app=test-nginx -ncnvrg --timeout=600s"
+#         res = self.exec_cmd(cmd)
+#         self.assertEqual(0, res[0])
+
+
+class CnvrgTaintsAreSetDedicatedNodesTrueTest(unittest.TestCase, CommonBase):
 
     @classmethod
     def setUpClass(cls):
         cls.get_nip_nip_url()
         cls.deploy()
         cls._exec_cmd("kubectl label nodes cnvrg-taint=true --all --overwrite")
-        cls.create_cnvrg_spec(CNVRG_SPEC.replace("__CLUSTER_DOMAIN__", cls.get_nip_nip_url()))
+        cls._exec_cmd("kubectl taint nodes cnvrg-taint=true:NoSchedule --all")
+        cls._exec_cmd("kubectl create deployment --image=nginx -ncnvrg test-nginx")
+        cls.create_cnvrg_spec(CNVRG_SPEC_WITH_TOLERATION.replace("__CLUSTER_DOMAIN__", cls.get_nip_nip_url()))
         cls.wait_for_cnvrg_spec_ready()
 
     @classmethod
     def tearDownClass(cls):
         return
         cls._exec_cmd("kubectl label node cnvrg-taint- --all")
+        cls._exec_cmd("kubectl taint nodes cnvrg-taint=true:NoSchedule --all")
         cls.delete_cnvrg_spec()
         cls.undeploy()
+
+    def test_app(self):
+        v1 = client.CoreV1Api()
+        pod = v1.list_namespaced_pod("cnvrg", label_selector="app=app")
+        self.assertEqual(1, len(pod.items))
+        self.assertEqual("Running", pod.items[0].status.phase)
 
     def test_pg(self):
         v1 = client.CoreV1Api()
@@ -185,3 +359,39 @@ class CnvrgTaintsAreSetDedicatedNodesFalseTest(unittest.TestCase, CommonBase):
         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=routing-service")
         self.assertEqual(1, len(pod.items))
         self.assertEqual("Running", pod.items[0].status.phase)
+
+    def test_custom_nginx_deploy(self):
+        v1 = client.CoreV1Api()
+        pod = v1.list_namespaced_pod("cnvrg", label_selector="app=test-nginx")
+        self.assertEqual(1, len(pod.items))
+        self.assertIsNotNone(pod.items[0].status.conditions[0].message)
+        self.assertIn("nodes are available", pod.items[0].status.conditions[0].message)
+
+#
+# class CnvrgTaintsAreSetDedicatedNodesTrueIstioOnlyTest(unittest.TestCase, CommonBase):
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.get_nip_nip_url()
+#         cls.deploy()
+#         cls._exec_cmd("kubectl label nodes cnvrg-taint=true --all --overwrite")
+#         cls._exec_cmd("kubectl taint nodes cnvrg-taint=true:NoSchedule --all")
+#         cls._exec_cmd("kubectl create deployment --image=nginx -ncnvrg test-nginx")
+#         cls.create_cnvrg_spec(
+#             CNVRG_SPEC_WITH_TOLERATION_ISTIO_ONLY.replace("__CLUSTER_DOMAIN__", cls.get_nip_nip_url()))
+#         cls.wait_for_cnvrg_spec_ready()
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         return
+#         cls._exec_cmd("kubectl label node cnvrg-taint- --all")
+#         cls._exec_cmd("kubectl taint nodes cnvrg-taint- --all")
+#         cls.delete_cnvrg_spec()
+#         cls.undeploy()
+#
+#     def test_istio_deployment(self):
+#
+#         v1 = client.CoreV1Api()
+#         pod = v1.list_namespaced_pod("cnvrg", label_selector="app=istiod")
+#         self.assertEqual(1, len(pod.items))
+#         self.assertEqual("Running", pod.items[0].status.phase)
