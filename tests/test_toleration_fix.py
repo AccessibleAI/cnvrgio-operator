809b375de770170e756102b23bc926b3e12ea822
Eli Lasry <mresl@ip-192-168-1-16.eu-west-1.compute.internal>
2020-10-22 19:58:55 +0300

    toleration tests

--- tests/test_toleration_fix.py
+++ tests/test_toleration_fix.py
@@ -3,10 +3,10 @@ import os
 from common import CommonBase
 from kubernetes import client, config
 import logging
+import time
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
-
 node_list = v1.list_node()
 nodes = []
 for obj in node_list.items:
@@ -54,55 +54,41 @@ class CnvrgEsSmallFixTest(unittest.TestCase, CommonBase):
 
     @classmethod
     def setUpClass(cls):
+        cls._exec_cmd("kubectl taint node {} kubernetes.azure.com/scalesetpriority=spot:NoSchedule --overwrite".format(nodes[-1]))
+        cls._exec_cmd("kubectl taint node {} nvidia.com/gpu=present:NoSchedule --overwrite".format(nodes[-1]))
+        cls._exec_cmd("kubectl label node {} accelerator=nvidia --overwrite".format(nodes[-1]))
         cls.deploy()
         cls.create_cnvrg_spec(CNVRG_SPEC)
         cls.wait_for_cnvrg_spec_ready()
+        time.sleep(3)
 
     @classmethod
     def tearDownClass(cls):
         if os.getenv("RUN_TEARDOWN", "true") == "true":
             cls.delete_cnvrg_spec()
             cls.undeploy()
-
-    def test_taint_node(self):
-        cmd1 = "kubectl -n cnvrg taint node {} kubernetes.azure.com/scalesetpriority=spot:NoExecute".format(nodes[-1])
-        cmd2 = "kubectl -n cnvrg taint node {} nvidia.com/gpu=present:NoExecute".format(nodes[-1])
-        cmd3 = "kubectl -n cnvrg label node {} accelerator=nvidia".format(nodes[-1])
-        res1 = self.exec_cmd(cmd1)
-        res2 = self.exec_cmd(cmd2)
-        res3 = self.exec_cmd(cmd3)
-        res4 = res1[0] + res2[0] + res3[0]
-        self.assertEqual(0, res4)
+            cls._exec_cmd("kubectl taint node {} kubernetes.azure.com/scalesetpriority-".format(nodes[-1]))
+            cls._exec_cmd("kubectl taint node {} nvidia.com/gpu-".format(nodes[-1]))
+            cls._exec_cmd("kubectl label node {} accelerator-".format(nodes[-1]))
 
     def test_fluentd_ready(self):
-        cmd = "kubectl -n cnvrg wait --for=condition=ready pod -l k8s-app=fluentd-logging --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
+        cmd = "kubectl -n cnvrg wait --for=condition=PodScheduled pod -l k8s-app=fluentd-logging --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
 
     def test_dcgm_ready(self):
-        cmd = "kubectl -n cnvrg wait --for=condition=ready pod -l app.kubernetes.io/name=dcgm-exporter --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
+        cmd = "kubectl -n cnvrg wait --for=condition=PodScheduled pod -l app.kubernetes.io/name=dcgm-exporter --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
 
 
     def test_node_exporter_ready(self):
-        cmd = "kubectl -n cnvrg wait --for=condition=ready pod -l app.kubernetes.io/name=node-exporter --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
+        cmd = "kubectl -n cnvrg wait --for=condition=PodScheduled pod -l app.kubernetes.io/name=node-exporter --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
 
 
     def test_nvidia_plugin_ready(self):
-        cmd = "kubectl -n cnvrg wait --for=condition=ready pod -l app=nvidia-device-plugin --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
+        cmd = "kubectl -n cnvrg wait --for=condition=PodScheduled pod -l app=nvidia-device-plugin --field-selector=spec.nodeName={}  --timeout=120s".format(nodes[-1])
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
-
-
-    def test_untaint_node(self):
-        cmd1 = "kubectl -n cnvrg taint node {} kubernetes.azure.com/scalesetpriority=spot:NoSchedule-".format(nodes[-1])
-        cmd2 = "kubectl -n cnvrg taint node {} nvidia.com/gpu=present:NoSchedule-".format(nodes[-1])
-        cmd3 = "kubectl -n cnvrg label node {} accelerator-".format(nodes[-1])
-        res1 = self.exec_cmd(cmd1)
-        res2 = self.exec_cmd(cmd2)
-        res3 = self.exec_cmd(cmd3)
-        res4 = res1[0] + res2[0] + res3[0]
-        self.assertEqual(0, res4)
