dcf8b48eb2d2df5596613fa91239d2ab82c364ca
MrEli <eslasry@gmail.com>
2020-12-09 13:28:58 +0200

    elastalert

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -339,6 +339,11 @@ class CnvrgTaintsAreSetDedicatedNodesFalseTest(unittest.TestCase, CommonBase):
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
 
+    def test_elastalert(self):
+        cmd = "kubectl wait --for=condition=ready pod -l app=elastalert -ncnvrg --timeout=300s"
+        res = self.exec_cmd(cmd)
+        self.assertEqual(0, res[0])
+
     def test_kibana(self):
         cmd = "kubectl wait --for=condition=ready pod -l app=kibana -ncnvrg --timeout=300s"
         res = self.exec_cmd(cmd)
@@ -435,6 +440,11 @@ class CnvrgTaintsAreSetDedicatedNodesTrueTest(unittest.TestCase, CommonBase):
 
     def test_es(self):
         cmd = "kubectl wait --for=condition=ready pod -l app=elasticsearch -ncnvrg --timeout=300s"
+        res = self.exec_cmd(cmd)
+        self.assertEqual(0, res[0])
+
+    def test_elastalert(self):
+        cmd = "kubectl wait --for=condition=ready pod -l app=elastalert -ncnvrg --timeout=300s"
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
 
@@ -625,6 +635,11 @@ class CnvrgTaintsAreSetDedicatedNodesTrueHostpathTest(unittest.TestCase, CommonB
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
 
+    def test_elastalert(self):
+        cmd = "kubectl wait --for=condition=ready pod -l app=elastalert -ncnvrg --timeout=300s"
+        res = self.exec_cmd(cmd)
+        self.assertEqual(0, res[0])
+
     def test_grafana(self):
         cmd = "kubectl wait --for=condition=ready pod -l app=grafana -ncnvrg --timeout=300s"
         res = self.exec_cmd(cmd)
@@ -682,29 +697,5 @@ class CnvrgTaintsAreSetDedicatedNodesTrueHostpathTest(unittest.TestCase, CommonB
 
     def test_vpa_updater(self):
         cmd = "kubectl wait --for=condition=ready pod -l app=vpa-updater -ncnvrg --timeout=300s"
-        res = self.exec_cmd(cmd)
-        self.assertEqual(0, res[0])
-
-
-class TaintsOperatorDeploymentTest(unittest.TestCase, CommonBase):
-    @classmethod
-    def setUpClass(cls):
-        logging.info("starting -> TaintsOperatorDeploymentTest")
-        cls._started_at = time.time()
-        cls._exec_cmd("kubectl create ns cnvrg")
-        cls._exec_cmd("kubectl label nodes cnvrg-taint=true --all --overwrite")
-        cls._exec_cmd("kubectl taint nodes cnvrg-taint=true:NoSchedule --all")
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._exec_cmd("kubectl label node cnvrg-taint- --all")
-        cls._exec_cmd("kubectl taint nodes cnvrg-taint- --all")
-        cls.undeploy()
-        cls.log_total_test_execution_time(cls._started_at, "TaintsOperatorDeploymentTest")
-
-    def test_app_ready_aks_istio(self):
-        cmd = 'helm template chart --set tenancy.enabled="true" --include-crds --no-hooks | VERSION=$TAG envsubst | kubectl create -f -'
-        self.exec_cmd(cmd)
-        cmd = "kubectl wait --for=condition=ready pod -l control-plane=cnvrg-operator -ncnvrg --timeout=300s"
         res = self.exec_cmd(cmd)
         self.assertEqual(0, res[0])
